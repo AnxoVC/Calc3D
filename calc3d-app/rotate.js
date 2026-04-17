5b368b80f114151b12cb889e98d9c978656f5e27
anxovc <vigoanxo000@gmail.com>
2026-04-17 20:36:23 +0200

    chore: remove rotate scripts

--- calc3d-app/rotate.js
+++
@@ -1,8 +0,0 @@
-const { Jimp } = require('jimp');
-async function main() {
-  const image = await Jimp.read('public/logo.png');
-  image.rotate(180);
-  await image.write('public/logo.png');
-  console.log('Rotated 180 degrees');
-}
-main().catch(console.error);
--- rotate.js
+++
@@ -1,8 +0,0 @@
-const { Jimp } = require('jimp');
-async function main() {
-  const image = await Jimp.read('calc3d-app/public/logo.png');
-  image.rotate(180);
-  await image.write('calc3d-app/public/logo.png');
-  console.log('Rotated 180 degrees');
-}
-main().catch(console.error);
